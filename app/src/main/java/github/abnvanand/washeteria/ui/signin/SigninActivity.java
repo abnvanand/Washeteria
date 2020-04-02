e70dc86b19ae53c80c05e17298b0c8d06e891792
Abhinav Anand <abnvanand@gmail.com>
2020-04-02 23:24:40 +0530

    Add dummy SigninActivity

---
+++ app/src/main/java/github/abnvanand/washeteria/ui/signin/SigninActivity.java
@@ -0,0 +1,16 @@
+package github.abnvanand.washeteria.ui.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import github.abnvanand.washeteria.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
