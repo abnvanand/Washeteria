86d1fae93600fcc9f76a798b8a9da0d60a4f43b6
Abhinav Anand <abnvanand@gmail.com>
2020-04-02 23:25:54 +0530

    Use getLocations and getMachines to populate UI

--- app/src/main/java/github/abnvanand/washeteria/MainActivity.java
+++ app/src/main/java/github/abnvanand/washeteria/MainActivity.java
@@ -1,26 +1,48 @@
 package github.abnvanand.washeteria;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.alamkanak.weekview.WeekView;
-import com.alamkanak.weekview.WeekViewDisplayable;
 import com.google.android.material.internal.NavigationMenu;
 
-import java.util.ArrayList;
-import java.util.Calendar;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
+import github.abnvanand.washeteria.data.adapters.MachineAdapter;
+import github.abnvanand.washeteria.data.model.Location;
+import github.abnvanand.washeteria.data.model.Machine;
+import github.abnvanand.washeteria.network.RetrofitSingleton;
+import github.abnvanand.washeteria.network.WebService;
+import github.abnvanand.washeteria.ui.signin.SigninActivity;
 import io.github.yavski.fabspeeddial.FabSpeedDial;
 import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener {
+
+    private SimpleMenuListenerAdapter menuListener;
+    MachineAdapter machineAdapter;
+    RecyclerView recyclerView;
+    ProgressDialog progressDialog;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FabSpeedDial fab = findViewById(R.id.fabSpeedDial);
-        fab.setMenuListener(new SimpleMenuListenerAdapter() {
+        menuListener = new SimpleMenuListenerAdapter() {
             @Override
             public boolean onPrepareMenu(NavigationMenu navigationMenu) {
                 // TODO: Do something with yout menu items, or return false if you don't want to show them
@@ -46,7 +68,67 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return false;
             }
+        };
+        fab.setMenuListener(menuListener);
+
+        spinner = (Spinner) findViewById(R.id.locationSelector);
+        spinner.setOnItemSelectedListener(this);
+
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        WebService webService = RetrofitSingleton.getRetrofitInstance()
+                .create(WebService.class);
+        Call<List<Location>> call = webService.getLocations();
+        call.enqueue(new Callback<List<Location>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Location>> call,
+                                   @NotNull Response<List<Location>> response) {
+                progressDialog.dismiss();
+                // Spinner Drop down elements
+                List<Location> body = response.body();
+
+                generateLocationList(body);
+                Timber.d("body.toString()%s", body.toString());
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Location>> call,
+                                  @NotNull Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Err!!!", Toast.LENGTH_SHORT)
+                        .show();
+                Timber.d(t.getLocalizedMessage());
+            }
         });
+    }
+
+    private void generateLocationList(List<Location> locations) {
+        recyclerView = findViewById(R.id.recyclerView);
+        // Creating adapter for spinner
+        ArrayAdapter<Location> locationArrayAdapter =
+                new ArrayAdapter<>(this,
+                        android.R.layout.simple_spinner_item,
+                        locations);
+
+        // Drop down layout style - list view with radio button
+        locationArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(locationArrayAdapter);
+
+        Timber.d("locationArrayAdapter: %s", locationArrayAdapter);
+    }
+
+    private void generateMachineList(List<Machine> machines) {
+        recyclerView = findViewById(R.id.recyclerView);
+        machineAdapter = new MachineAdapter(this, machines);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        Timber.d("machineAdapter: %s", machineAdapter);
+        recyclerView.setAdapter(machineAdapter);
     }
 
     @Override
@@ -65,9 +147,53 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(
+                    new Intent(MainActivity.this,
+                            SigninActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        Location item = (Location) parent.getItemAtPosition(position);
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+        // Call to get machines of selected location
+        WebService webService = RetrofitSingleton.getRetrofitInstance()
+                .create(WebService.class);
+
+        progressDialog.show();
+        Call<List<Machine>> call = webService.getMachines(item.getLocationId());
+        call.enqueue(new Callback<List<Machine>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Machine>> call,
+                                   @NotNull Response<List<Machine>> response) {
+                progressDialog.dismiss();
+                List<Machine> body = response.body();
+                generateMachineList(body);
+                Timber.d("body.toString()%s", body.toString());
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Machine>> call,
+                                  @NotNull Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Err!!!", Toast.LENGTH_SHORT)
+                        .show();
+                Timber.d(t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ app/src/main/java/github/abnvanand/washeteria/data/adapters/MachineAdapter.java
@@ -0,0 +1,60 @@
+package github.abnvanand.washeteria.data.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import github.abnvanand.washeteria.R;
+import github.abnvanand.washeteria.data.model.Machine;
+
+public class MachineAdapter extends RecyclerView.Adapter<MachineAdapter.CustomViewHolder> {
+
+    private List<Machine> dataList;
+    private Context context;
+
+    public MachineAdapter(Context context, List<Machine> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        public final View mView;
+        TextView machineName;
+        TextView machineStatus;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+            machineName = mView.findViewById(R.id.textViewMachineName);
+            machineStatus = mView.findViewById(R.id.textViewMachineStatus);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MachineAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.list_item_live_status, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MachineAdapter.CustomViewHolder holder, int position) {
+        holder.machineName.setText(dataList.get(position).getMachineId());
+        holder.machineStatus.setText(dataList.get(position).getMachineName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
