28035a9cc42a6d825b6d543b1dcd2839c4e0dbbf
Abhinav Anand <abnvanand@gmail.com>
2020-04-09 00:13:59 +0530

    Add Okhttp logging interceptor

--- app/src/main/java/github/abnvanand/washeteria/network/RetrofitSingleton.java
+++ app/src/main/java/github/abnvanand/washeteria/network/RetrofitSingleton.java
@@ -1,6 +1,8 @@
 package github.abnvanand.washeteria.network;
 
 import github.abnvanand.washeteria.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,9 +14,20 @@ public class RetrofitSingleton {
 
     public static Retrofit getRetrofitInstance() {
         if (instance == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            // Set desired log level NONE, BASIC, HEADERS or body
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            // add your other interceptors
+            // add logging as last interceptor
+            httpClient.addInterceptor(logging);
+
+
             instance = new Retrofit.Builder()
                     .baseUrl(BuildConfig.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
 
