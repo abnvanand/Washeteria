8f81238b7dc0983e4dd650ea3b6e28ee1a4205cd
Abhinav Anand <abnvanand@gmail.com>
2020-04-17 00:09:27 +0530

    Add time picker to reserve slot activity

--- app/src/main/java/github/abnvanand/washeteria/ui/events/ReserveSlotActivity.java
+++ app/src/main/java/github/abnvanand/washeteria/ui/events/ReserveSlotActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -37,6 +43,11 @@ public class ReserveSlotActivity extends AppCompatActivity {
     EventCreateBody eventCreateBody;
     String token;
     String username;
+    Calendar startsAt;
+    Calendar endsAt;
+    EditText startsAtEditText;
+    EditText endsAtEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +55,11 @@ public class ReserveSlotActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reserve_slot);
 
         TextView machineNameTV = findViewById(R.id.machineName);
-        EditText startsAtDate = findViewById(R.id.startsAtDate);
-        EditText startsAtTime = findViewById(R.id.startsAtTime);
-        EditText endsAtDate = findViewById(R.id.endsAtDate);
-        EditText endsAtTime = findViewById(R.id.endsAtTime);
+        startsAtEditText = findViewById(R.id.startsAtDate);
+        endsAtEditText = findViewById(R.id.endsAtDate);
+        ImageButton editStartsAt = findViewById(R.id.btnEditStartsAt);
+        ImageButton editEndsAt = findViewById(R.id.btnEditEndsAt);
+
         Button btnSubmit = findViewById(R.id.btnReserveSlot);
 
         String machineId = getIntent().getStringExtra(EXTRA_MACHINE_ID);
@@ -56,10 +68,11 @@ public class ReserveSlotActivity extends AppCompatActivity {
         if (millis == -1)
             Timber.wtf("This is fucked up");
 
-        Calendar startsAt = Calendar.getInstance();
+
+        startsAt = Calendar.getInstance();
         startsAt.setTime(new Date(millis));
 
-        Calendar endsAt = (Calendar) startsAt.clone();
+        endsAt = (Calendar) startsAt.clone();
         endsAt.add(Calendar.MINUTE, SLOT_MAX_LIMIT_MINUTES);
 
         executor.execute(() -> {
@@ -90,8 +103,9 @@ public class ReserveSlotActivity extends AppCompatActivity {
             });
         });
 
-        startsAtDate.setText(startsAt.getTime().toString());
-        endsAtDate.setText(endsAt.getTime().toString());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.getDefault());
+        startsAtEditText.setText(dateFormat.format(startsAt.getTime()));
+        endsAtEditText.setText(dateFormat.format(endsAt.getTime()));
 
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -132,5 +146,55 @@ public class ReserveSlotActivity extends AppCompatActivity {
                         });
             }
         });
+
+        editStartsAt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                picker("start");
+            }
+        });
+
+        editEndsAt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                picker("end");
+            }
+        });
+    }
+
+
+    void picker(String type) {
+        final View dialogView = View.inflate(this, R.layout.date_time_picker, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+
+        dialogView.findViewById(R.id.btn_set)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
+
+                        Calendar newCalendar = new GregorianCalendar(startsAt.get(Calendar.YEAR),
+                                startsAt.get(Calendar.MONTH),
+                                startsAt.get(Calendar.DAY_OF_MONTH),
+                                timePicker.getCurrentHour(),
+                                timePicker.getCurrentMinute());
+
+                        alertDialog.dismiss();
+                        if (type.equalsIgnoreCase("start")) {
+                            startsAt = newCalendar;
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.getDefault());
+
+                            startsAtEditText.setText(dateFormat.format(startsAt.getTime()));
+                        } else if (type.equalsIgnoreCase("end")) {
+                            endsAt = newCalendar;
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.getDefault());
+                            endsAtEditText.setText(dateFormat.format(endsAt.getTime()));
+                        }
+                    }
+                });
+        alertDialog.setView(dialogView);
+        alertDialog.show();
     }
 }
