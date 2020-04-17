2fa217b21d853bc71bab986b7b211d1fbdb094ec
Abhinav Anand <abnvanand@gmail.com>
2020-04-17 09:53:26 +0530

    Fix BookingEvent pojo's datatypes

--- app/src/main/java/github/abnvanand/washeteria/ui/events/BookingEvent.java
+++ app/src/main/java/github/abnvanand/washeteria/ui/events/BookingEvent.java
@@ -1,15 +1,21 @@
 package github.abnvanand.washeteria.ui.events;
 
+import android.graphics.Color;
+
 import com.alamkanak.weekview.WeekViewDisplayable;
 import com.alamkanak.weekview.WeekViewEvent;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Calendar;
 
+import github.abnvanand.washeteria.R;
+
 public class BookingEvent implements WeekViewDisplayable<BookingEvent> {
-    private long id;
+    private int id;
     private Calendar startsAt;
     private Calendar endsAt;
-    boolean cancelled;
+    private boolean cancelled;
     private String machineName;
     private String creator;
     private Integer color;
@@ -18,13 +24,12 @@ public class BookingEvent implements WeekViewDisplayable<BookingEvent> {
     }
 
 
-    public BookingEvent(long id,
-                        Calendar startsAt,
-                        Calendar endsAt,
-                        boolean cancelled,
-                        String machineName,
-                        String creator,
-                        Integer color) {
+    BookingEvent(int id,
+                 Calendar startsAt, Calendar endsAt,
+                 boolean cancelled,
+                 String machineName,
+                 String creator,
+                 Integer color) {
         this.id = id;
         this.startsAt = startsAt;
         this.endsAt = endsAt;
@@ -34,11 +39,11 @@ public class BookingEvent implements WeekViewDisplayable<BookingEvent> {
         this.color = color;
     }
 
-    public long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -90,11 +95,19 @@ public class BookingEvent implements WeekViewDisplayable<BookingEvent> {
         this.color = color;
     }
 
+    @NotNull
     @Override
-    public WeekViewEvent toWeekViewEvent() {
+    public WeekViewEvent<BookingEvent> toWeekViewEvent() {
+        int backgroundColor = (!cancelled) ? color : Color.WHITE;
+        int textColor = !cancelled ? Color.WHITE : color;
+        int borderWidthResId = (!cancelled) ? R.dimen.no_border_width : R.dimen.border_width;
+
         WeekViewEvent.Style style = new WeekViewEvent.Style.Builder()
-                .setBackgroundColor(color)
+                .setTextColor(textColor)
+                .setBackgroundColor(backgroundColor)
                 .setTextStrikeThrough(cancelled)
+                .setBorderWidthResource(borderWidthResId)
+                .setBorderColor(color)
                 .build();
 
         return new WeekViewEvent.Builder<>(this)
