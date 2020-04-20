258b3da6eaed06d64023962794c9c72f58043dec
Abhinav Anand <abnvanand@gmail.com>
2020-04-20 17:15:58 +0530

    Add resource class which wrap data along with status
    Status can be one on LOADING, SUCCESS, ERROR

---
+++ app/src/main/java/github/abnvanand/washeteria/models/pojo/Resource.java
@@ -0,0 +1,58 @@
+package github.abnvanand.washeteria.models.pojo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Resource<T> {
+    private Status status;
+    private T data;
+    private String msg;
+
+    public Resource(){
+
+    }
+
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    private Resource(@NonNull Status status, @Nullable T data, @Nullable String msg) {
+        this.status = status;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public Resource<T> success(T data) {
+        return new Resource<>(Status.SUCCESS, data, null);
+    }
+
+    public Resource<T> error(String msg, T data) {
+        return new Resource<>(Status.ERROR, data, msg);
+    }
+
+    public Resource<T> loading(T data) {
+        return new Resource<>(Status.LOADING, data, null);
+    }
+
+}
---
+++ app/src/main/java/github/abnvanand/washeteria/models/pojo/Status.java
@@ -0,0 +1,7 @@
+package github.abnvanand.washeteria.models.pojo;
+
+public enum Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
